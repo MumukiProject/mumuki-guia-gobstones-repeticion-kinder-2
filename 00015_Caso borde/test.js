4cbcc88173820930f648e3579d20d3f0215409a7
rocio@mumuki.org <rocio@mumuki.org>
2021-01-08 20:44:57 +0000

    Mumuki Export on 2021-01-08 20:44:57 +0000

--- 00015_Caso borde/test.js
+++ 00015_Caso borde/test.js
@@ -1,4 +1,4 @@
 const baseUrl = 'https://raw.githubusercontent.com/MumukiProject/mumuki-guia-gobstones-repeticion-kinder-2/master/assets/';
-Muzzle.aspectRatio = 0.43
+Muzzle.aspectRatio = 0.44
 //Muzzle.strokeWidth = 6;
 Muzzle.choose(`${baseUrl}/match15_prog_si-01_1610138527826.svg` , `${baseUrl}/match15_tab-01_1610138588072.svg`, [`${baseUrl}/match15_prog_no-01_1610138563275.svg`], 1);
