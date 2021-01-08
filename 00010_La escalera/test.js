077e4ca96d81242e6e231db12ee80a9d1895c10e
rocio@mumuki.org <rocio@mumuki.org>
2021-01-08 20:59:19 +0000

    Mumuki Export on 2021-01-08 20:59:19 +0000

--- 00010_La escalera/test.js
+++ 00010_La escalera/test.js
@@ -1,4 +1,4 @@
 const baseUrl = 'https://raw.githubusercontent.com/MumukiProject/mumuki-guia-gobstones-repeticion-kinder-2/master/assets/';
-Muzzle.aspectRatio = 4 / 10
+Muzzle.aspectRatio = 0.5
 //Muzzle.strokeWidth = 6;
-Muzzle.choose(`${baseUrl}/match10_prog_si_1610138325940.svg` , `${baseUrl}/match15_tab-01_1610139271577.svg`, [`${baseUrl}/match10_prog_no_1610138288233.svg`], 1);
+Muzzle.choose(`${baseUrl}/match10_prog_si_1610138325940.svg` , `${baseUrl}/match10_tab-01_1610138242299.svg`, [`${baseUrl}/match10_prog_no_1610138288233.svg`], 1);
